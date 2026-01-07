570484f9ce8358ce3cafe7a2b8133282323783b7
Vezhnovets I <vigorn@mail.ru>
2026-01-07 18:38:25 +0300

    prom

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from routers import redis_router
 from logging_config import setup_logging
 from middlewares.logging import log_requests
-
-
 import logging
 from fastapi import Request
+from middlewares.metrics import metrics_middleware
+
+from fastapi import Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+
 
 logger = logging.getLogger("errors")
 
@@ -13,11 +16,19 @@ setup_logging()
 
 app = FastAPI(title="Multi-Service API", version="1.0.0")
 app.middleware("http")(log_requests)
+app.middleware("http")(metrics_middleware)
 
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
     logger.exception("Unhandled error: %s %s", request.method, request.url.path)
     raise exc
+
+@app.get("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        media_type=CONTENT_TYPE_LATEST
+    )
 
 app.include_router(redis_router.router)
 #app.include_router(rabbitmq_router.router)
--- services/redis_service.py
+++ services/redis_service.py
@@ -2,6 +2,9 @@ from redis.asyncio.cluster import RedisCluster
 from config import config
 import logging
 from utils.logging_decorators import log_call
+from metrics import SERVICE_CALLS
+from utils.metrics_decorator import service_metric
+
 
 logger = logging.getLogger(__name__)
 
@@ -10,12 +13,26 @@ redis_client = RedisCluster.from_url(
     decode_responses=True
 )
 
+@service_metric("redis")
 @log_call("redis")
 async def write_to_redis(key: str, value: str):
+
+    SERVICE_CALLS.labels(
+        service="redis",
+        function="write"
+    ).inc()
+
     await redis_client.set(key, value)
     return {"status": "written"}
 
+@service_metric("redis")
 @log_call("redis")
 async def read_from_redis(key: str):
+
+    SERVICE_CALLS.labels(
+        service="redis",
+        function="read"
+    ).inc()
+
     value = await redis_client.get(key)
     return {"value": value if value else None}
