1698f538a6d8243063798cdcede11e80f99abc8b
Vezhnovets I <vigorn@mail.ru>
2026-01-08 19:30:07 +0300

    prom

--- logging_config.py
+++ logging_config.py
@@ -1,14 +1,47 @@
-import logging
-import sys
+import logging.config
 
-LOG_FORMAT = (
-    "%(asctime)s | %(levelname)s | %(name)s | "
-    "%(funcName)s:%(lineno)d | %(message)s"
-)
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
 
-def setup_logging(level=logging.INFO):
-    logging.basicConfig(
-        level=level,
-        format=LOG_FORMAT,
-        handlers=[logging.StreamHandler(sys.stdout)],
-    )
+    "formatters": {
+        "default": {
+            "format": (
+                "%(asctime)s | %(levelname)s | %(name)s | "
+                "%(funcName)s:%(lineno)d | %(message)s"
+            ),
+        },
+    },
+
+    "handlers": {
+        "default": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+
+    "loggers": {
+        "": {
+            "handlers": ["default"],
+            "level": "INFO",
+        },
+        "uvicorn": {
+            "handlers": ["default"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "uvicorn.access": {
+            "handlers": ["default"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "uvicorn.error": {
+            "handlers": ["default"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
+def setup_logging():
+    logging.config.dictConfig(LOGGING_CONFIG)
