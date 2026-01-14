6433f7ebe1fda20d56e6618847f6bf104fde95cc
Vezhnovets I <vigorn@mail.ru>
2026-01-14 21:53:10 +0300

    update redis

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ CONSUL_ACCESS_READ = os.getenv("CONSUL_ACCESS_READ", "service-check")
 
 class RedisConfig(BaseModel):
     url: str
+    password: str | None = None
 
 
 class RabbitMQConfig(BaseModel):
