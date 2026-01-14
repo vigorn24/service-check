96ad3b46262367671da48d2443a57706369a0e3e
invezhno <invezhno@mts.ru>
2026-01-14 17:15:42 +0300

    add swagger

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from middlewares.metrics import metrics_middleware
 
 from fastapi import Response
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 logger = logging.getLogger("errors")
 
 setup_logging()
 
-app = FastAPI(title="Multi-Service API", version="1.0.0")
+app = FastAPI(title="Multi-Service API", version="1.0.0", docs_url=None, redoc_url=None, openapi_url="/openapi.json")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.middleware("http")(log_requests)
 app.middleware("http")(metrics_middleware)
 
@@ -29,6 +32,37 @@ def metrics():
         generate_latest(),
         media_type=CONTENT_TYPE_LATEST
     )
+
+@app.get("/docs", include_in_schema=False)
+def custom_swagger_ui():
+    return HTMLResponse("""
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Swagger UI</title>
+  <link rel="stylesheet" type="text/css" href="/static/swagger/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="/static/swagger/swagger-ui-bundle.js"></script>
+  <script src="/static/swagger/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = () => {
+      SwaggerUIBundle({
+        url: "/openapi.json",
+        dom_id: "#swagger-ui",
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout"
+      });
+    };
+  </script>
+</body>
+</html>
+""")
+
 
 app.include_router(redis_router.router)
 #app.include_router(rabbitmq_router.router)
