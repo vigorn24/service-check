3cfed0fc98a59573db8acbf7b4f8657fd8179c7d
invezhno <invezhno@mts.ru>
2026-01-29 22:36:02 +0300

    update 14

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
+
+
 Запуск сервиса
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 
--- services/redis_service.py
+++ services/redis_service.py
@@ -4,7 +4,9 @@ import logging
 from utils.logging_decorators import log_call
 from metrics import SERVICE_CALLS
 from utils.metrics_decorator import service_metric
-
+from redis.exceptions import (RedisError, ConnectionError as RedisConnectionError, TimeoutError as RedisTimeoutError)
+from fastapi import HTTPException
+import asyncio
 
 logger = logging.getLogger(__name__)
 
@@ -16,15 +18,25 @@ redis_client = RedisCluster.from_url(
 
 @service_metric("redis")
 @log_call("redis")
-async def write_to_redis(key: str, value: str):
+async def write_to_redis(key: str, value: str, expire_time: int = 3600): # Default expire_time to 1 hour
 
 #    SERVICE_CALLS.labels(
 #        service="redis",
 #        function="write"
 #    ).inc()
+    for attempt in range(3):
+        try:
+            await redis_client.set(key, value, ex=expire_time)
+            return {"status": "written"}
+        except (RedisConnectionError, RedisTimeoutError):
+            logger.warning("Redis connection issue, attempt %s", attempt + 1, exc_info=True)
+            await asyncio.sleep(0.1)
 
-    await redis_client.set(key, value)
-    return {"status": "written"}
+        except RedisError:
+            logger.exception("Redis error (non-retriable)")
+            break
+
+    raise HTTPException(status_code=503, detail="Redis service unavailable")
 
 @service_metric("redis")
 @log_call("redis")
