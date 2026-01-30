68d2d145630ea5bf66053d3d03f78790e3ccb236
invezhno <invezhno@mts.ru>
2026-01-30 12:24:50 +0300

    update base model

--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel, Field
 class DataModel(BaseModel):
     key: str = Field(..., min_length=1)
     value: str = Field(..., min_length=1)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "key": "devops-*",
+                "value": "Hello, World!"
+            }
+        }
